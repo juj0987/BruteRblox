788f494e3c83d2cc1646e511c955035582b70979
juj0987 <bilalyucel00@gmail.com>
2025-03-23 16:30:52 +0100

    Update pg.py

--- pg.py
+++ pg.py
@@ -30,7 +30,7 @@ def main():
 
     print("YOU CAN PROVIDE USERNAME NOW")
     # Get the user-specified username
-    username = input("Username: ")
+    username = input("Username:Leddion_17234 ")
 
     print("Getting password_list...")
     time.sleep(1)
